b0a7843c88bee5f638394486fa8df3089cb5f62e
gabrielcamarate <gabrielcamarate@icloud.com>
2026-03-20 08:26:57 -0300

    Adiciona scripts: `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NotTabNews
+
 Um clone experimental do TabNews criado durante o curso.dev — explorando aprendizado, código limpo e umas ideias próprias pelo caminho.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-function Home(){
-    return (
-        <h1>Amor, eu amo você! se você me ama da uma risadinha, e um beijo no Bryan</h1>
-    )
+function Home() {
+  return (
+    <h1>
+      Amor, eu amo você! se você me ama da uma risadinha, e um beijo no Bryan
+    </h1>
+  );
+}
+
+function test2() {
+  console.log("Configurando, prettier!");
 }
 
 export default Home;
